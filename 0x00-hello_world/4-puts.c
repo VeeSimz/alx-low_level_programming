48398c4d456e9391f779da3a0adeb5f78e2ae44b
VeeSimz <kayveesimba@gmail.com>
2023-08-10 13:04:19 -0700

    Hello puts correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /* writing code that prints without using the printf command*/
-int main ()
+int main (void)
 {
 puts ("Programming is like building a multilingual puzzle \n";)
 return (0);
