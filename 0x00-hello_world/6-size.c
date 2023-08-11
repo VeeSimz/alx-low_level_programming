c17dd96ec697f3cce1cfaf3dbeba9efdbcb066b8
VeeSimz <kayveesimba@gmail.com>
2023-08-10 18:17:44 -0700

    More size corrections

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,16 @@
  */
 int main(void)
 {
-	printf("Size of a char:%b byte(s)\n", sizeof(char));
-	printf("Size of an int:%b byte(s)\n", sizeof(int));
-	printf("Size of a long int:%b byte(s)\n", sizeof(long int));
-	printf("Size of a long long int:%b byte(s)\n", sizeof(long long int));
-	printf("Size of a float:%b byte(s)\n", sizeof(float));
-	return(0);
+	char a;
+	int i;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("Size of a char:%b byte(s)\n", sizeof(a));
+	printf("Size of an int:%b byte(s)\n", sizeof(i));
+	printf("Size of a long int:%b byte(s)\n", sizeof(c));
+	printf("Size of a long long int:%b byte(s)\n", sizeof(d));
+	printf("Size of a float:%b byte(s)\n", sizeof(f));
+	return (0);
 }
