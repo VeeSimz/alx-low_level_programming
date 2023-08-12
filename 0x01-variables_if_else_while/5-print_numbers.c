c684b586225ebe77e25832707f68f64f7e2b1042
VeeSimz <kayveesimba@gmail.com>
2023-08-12 01:53:38 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 10; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
