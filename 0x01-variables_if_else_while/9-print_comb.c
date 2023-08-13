3d8f7cc9a4ab25195344bad9777ee8753e356511
VeeSimz <kayveesimba@gmail.com>
2023-08-13 00:07:15 -0700

    Print number output

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,20 +8,15 @@ int main(void)
 {
 	int i;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		if (i == 9)
-		{
-			break;
-		}
-		putchar(i);
-		if (i + 1)
+		putchar(i + '0');
+		if (i != 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
-	putchar('\n');
 	return (0);
 }
 
