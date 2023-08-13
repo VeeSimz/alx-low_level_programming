54f3df7c7b3c0ab05fe98dd3a8df6beb45d8b5aa
VeeSimz <kayveesimba@gmail.com>
2023-08-13 01:22:21 -0700

    Number combinations

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - program that prints different combination of three digit numbers
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 48; i <= 55; i++)
+	{
+		for (j = 49; j <= 56; j++)
+		{
+			for (k = 50; k <= 57; k++)
+			{
+				if (k > j && j > i)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i != 55 || j != 56 || k != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
