ea777d6267ad69cbeb8a7381b77022e5599c1081
VeeSimz <kayveesimba@gmail.com>
2023-08-15 23:40:01 +0200

    Alphabet times 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,9 @@ void print_alphabet_x10(void)
 	char alph;
 	int a;
 
-	for (alph = 'a'; alph <= 'z'; alph++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (a = 0; a <= 9; a++)
+		for (alph = 'a'; alph <= 'z'; alph++)
 		{
 			_putchar(alph);
 		}
