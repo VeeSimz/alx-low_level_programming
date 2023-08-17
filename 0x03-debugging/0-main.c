b3a6b0fee7ac9347e2922abceb1a493b911c8fcc
VeeSimz <kayveesimba@gmail.com>
2023-08-17 15:56:30 +0200

    Positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * positive_or_negative - assign random number to variable
+ *
+ * Description: 'the program assignsrandon numbers to variable'
+ *
+ * Printf: outputs result
+ *
+ * Return: always (Success)
+ */
+int positive_or_negative(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	} else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	} else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
