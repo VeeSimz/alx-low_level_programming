379099b90d327a0d4c2cdcb2da8ad4f1e3cd0365
VeeSimz <kayveesimba@gmail.com>
2023-08-18 15:31:39 +0200

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * _isupper - checks if the character is uppercase
+ * @c: type of integer
+ * Return: 1 if c is uppercase else return 0
+ */
+int _isupper(int c);
+{
+	char Alph = 'C';
+	char alph = 'c';
+
+	if (_isupper(Alph)
+		_putchar("C is uppercase");
+			return (1);
+			else
+			_putchar("c is not uppercase");
+			return (0);
+}
+
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies two integers
+ * Return: 0
+ */
+
+int mul(int a, int b);
+{ 
