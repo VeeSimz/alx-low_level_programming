ca4aa1feb190b20a78aeded2fd52b21878c07e21
VeeSimz <kayveesimba@gmail.com>
2023-08-18 20:26:06 +0200

    Isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,16 +5,11 @@
  * @c: type of integer
  * Return: 1 if c is uppercase else return 0
  */
-int _isupper(int c);
+int _isupper(int c)
 {
-	char Alph = 'C';
-	char alph = 'c';
-
-	if (_isupper(Alph)
-		_putchar("C is uppercase");
-			return (1);
-			else
-			_putchar("c is not uppercase");
-			return (0);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
 
