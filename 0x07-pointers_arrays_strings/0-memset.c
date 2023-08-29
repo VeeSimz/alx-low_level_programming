30e36d5687997e9be2964596a5fb095d9a7ffc04
VeeSimz <kayveesimba@gmail.com>
2023-08-29 22:03:35 +0200

    Memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _memset - fills memory with a constant byte
+ * *_memset - fills memory with a constant byte
  * @s: pointer to memory area
  * @b: constant byte
  * @n: first byte
