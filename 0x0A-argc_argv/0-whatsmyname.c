ae0c3529a1fd0899fbb1d50716711f40b4d5a48d
VeeSimz <kayveesimba@gmail.com>
2023-09-02 04:21:50 +0200

    It aint the way they call you

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,13 +5,10 @@
  * @argv: argument vector
  * Return: 0
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	int i;
+	(void) argc;
 
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s \n", argv[0]);
-	}
+	printf("%s \n", argv[0]);
 	return (0);
 }
