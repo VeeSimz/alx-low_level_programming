ebd33d01b188bbe41252b3ad9cf5b66df1db7a90
VeeSimz <kayveesimba@gmail.com>
2023-09-06 15:59:38 +0200

    Argstostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -7,14 +7,14 @@
  * Return: pointer to a new string
  */
 
-char *argstostr(int ac, char **av)
+char *argstostr(int ac, char *av[])
 {
 	char *str;
 
 	str = argstostr(ac, av);
 	if (str == NULL)
 		return (NULL);
-	printf("%s", str);
+	printf("%s\n", str);
 	free(str);
 	return (str);
 }
