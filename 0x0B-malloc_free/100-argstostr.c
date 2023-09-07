28d07316810919a445d8d1d5fef3d9e833ebd3e8
VeeSimz <kayveesimba@gmail.com>
2023-09-07 02:53:41 +0200

    Argstostr corrections

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * argstostr - concatenates arguments
@@ -7,14 +9,26 @@
  * Return: pointer to a new string
  */
 
-char *argstostr(int ac, char *av[])
+char *argstostr(int ac, char **av)
 {
 	char *str;
+	int i, j, k, strlen;
 
-	str = argstostr(ac, av);
+	while (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+		for (j = 0; av[i][j] != '\0'; j++)
+			strlen++;
+	strlen += ac;
+	str = malloc(sizeof(char) * strlen + 1);
 	if (str == NULL)
 		return (NULL);
-	printf("%s\n", str);
+	for (i = 0; i < ac; i++)
+		for (j = 0; av[i][j] != '\0'; j++)
+			str[k] = av[i][j];
+			k++;
+	if (str[k] == '\0')
+		str[k++] = '\n';
 	free(str);
 	return (str);
 }
