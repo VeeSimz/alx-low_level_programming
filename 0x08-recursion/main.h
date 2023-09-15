f635bc741ec94faeab5cc2a373539560b5a66c16
VeeSimz <kayveesimba@gmail.com>
2023-09-15 23:31:59 +0200

    Palindrome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_h
+#define MAIN_H
+
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -7,3 +10,8 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int check_prime(int n, int i);
 int _putchar(char c);
+int is_palindrome(char *s);
+int check(char *s, int i, int len);
+int wildcmp(char *s1, char *s2);
+
+#endif
