f1b6a3c2364619a18700c4ab603987690ef7acdc
VeeSimz <kayveesimba@gmail.com>
2023-09-22 10:03:08 +0200

    The hare and the tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * first - prints a string
+ * Return: void
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
