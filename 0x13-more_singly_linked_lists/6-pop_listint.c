62ecbbb5b5b78740f693eaacd84cf88c132e4de3
VeeSimz <kayveesimba@gmail.com>
2023-09-27 00:05:55 +0200

    Pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,15 +9,14 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
+	int n;
 
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return (1);
-	else
-	{
-		temp = *head;
-		*head = (*head)->next;
-		free(temp);
-		temp = NULL;
-	}
-	return (0);
+	n = (*head)->n;
+	temp = *head;
+	*head = (*head)->next;
+	free(temp);
+
+	return (n);
 }
